1f333b96c2165a906252f19bcdbd3c7afdd1a3e4
Kevin Thomas <kevint@magenic.com>
2017-08-07 14:11:18 -0500

    Added instruction pages

--- src/app/shared/app.routing.ts
+++ src/app/shared/app.routing.ts
@@ -7,6 +7,8 @@ import { ErrorComponent } from '../error/error.component';
 import { RegisterComponent } from '../register/register.component';
 import { InstructionsComponent } from '../instructions/instructions.component';
 import { OptionsComponent } from '../options/options.component';
+import { LearnArenaComponent } from '../instructions/learnArena/learn-arena.component';
+import { LearnStoryComponent } from '../instructions/learnStory/learn-story.component';
 
 import { AppService } from '../shared/app.service';
 
@@ -18,6 +20,8 @@ import { AppService } from '../shared/app.service';
             { path: 'register' , component: RegisterComponent },
             { path: 'instructions' , component: InstructionsComponent, canActivate: [AppService] },
             { path: 'options' , component: OptionsComponent, canActivate: [AppService] },
+            { path: 'learn-arena' , component: LearnArenaComponent, canActivate: [AppService] },
+            { path: 'learn-story' , component: LearnStoryComponent, canActivate: [AppService] },
             { path: '**' , component: ErrorComponent }            
         ])
     ],
